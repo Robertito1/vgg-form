906fa6c015714749bcb8baa6414f6ea2bcc89286
robert <orazurobert@yahoo.com>
2020-01-23 00:15:03 +0000

    tit

---
+++ baba.js
@@ -0,0 +1,5 @@
+var fruits = [45, 4, 9, 16];
+ function myFunction(total, value, index, array) {
+    return total + value  ;}
+var numbers = fruits.reduce(myFunction);
+document.write("winning winning" + numbers );
