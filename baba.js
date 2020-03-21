30269ca9d1bb5ba85c2b27e251c611f7b10cc129
robert <orazurobert@yahoo.com>
2020-03-21 00:31:16 +0000

    task

--- baba.js
+++ baba.js
@@ -1,8 +1,18 @@
-var cars=["bmw","volvo","saAB", "FORD", "fiat", "audi"];
-var text="";
-var i;
-for (i=0;i<cars.length;i++){
-    text += cars[i] + "<br>";
-}
-document.write(text)
-localStorage.clear
+// var cars=["bmw","volvo","saAB", "FORD", "fiat", "audi"];
+// var text="";
+// var i;
+// for (i=0;i<cars.length;i++){
+//     text += cars[i] + "<br>";
+// }
+// document.write(text)
+// localStorage.clear
+// let a =[6,7,8,9];
+// const b = [6,7,8,9,];
+// a[2] = 9;
+// b[2] = 9;
+
+// console.log(a);
+// console.log(b);
+// console.log (a == b);
+
+
